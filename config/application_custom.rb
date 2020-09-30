8b07ca0f780fc25e7ec799eca09b69090aee8190
Pedro Markun <pedro@markun.com.br>
2020-09-29 23:08:43 -0300

    pt como lingua padrão

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :pt-BR
+    config.i18n.default_locale = :"pt-BR"
     available_locales = ["es","pt-BR"]
   end
 end
